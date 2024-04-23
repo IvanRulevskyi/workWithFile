4e095220671fdb53abef4b509643e1bfb891fc86
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-24 00:37:12 +0300

    workWithFile

---
+++ test/Function.cpp
@@ -0,0 +1,8 @@
+//
+//  Function.cpp
+//  test
+//
+//  Created by Иван Рулевский on 24.04.2024.
+//
+
+#include "Function.hpp"
---
+++ test/Function.hpp
@@ -0,0 +1,13 @@
+//
+//  Function.hpp
+//  test
+//
+//  Created by Иван Рулевский on 24.04.2024.
+//
+
+#ifndef Function_hpp
+#define Function_hpp
+
+#include <stdio.h>
+
+#endif /* Function_hpp */
